e5600e8f38b6d544018e0b6ef76c715c073cc0e7
Mutlu MUTLUTAN <mutlutan@outlook.com>
2021-12-01 02:36:29 +0300

    eklemeler

--- WebApp1/wwwroot/Areas/Nft/Comps/compProjectList.js
+++ WebApp1/wwwroot/Areas/Nft/Comps/compProjectList.js
@@ -16,7 +16,11 @@ function compProjectList(_elm, _opt) {
     function fnHtmlAppend() {
         var temp = `
             <div>
-                <div id="divGrid"></div>
+                <div>
+                    <a name="btnAddProject" class="btn btn-sm btn-link " type="button">AddProject</a>
+                </div>
+                <div id="divProjectList" class="border p-1">
+                </div>
             </div>
         `;
         self.$elm.append(temp);
@@ -34,7 +38,47 @@ function compProjectList(_elm, _opt) {
     }
 
     function fnCreateElements() {
+        $.ajax({
+            url: "/api/Nft/GetProjectList",
+            type: "GET", dataType: "json",
+            beforeSend: function (jqXHR, settings) {
+                setTimeout(function () {
+                    kendo.ui.progress(self.$elm, true); //progress On
+                });
+            },
+            complete: function (jqXHR, textStatus) {
+                setTimeout(function () {
+                    kendo.ui.progress(self.$elm, false); //progress Off
+                });
+            },
+            success: function (result, textStatus, jqXHR) {
+                if (result.Success == true) {
 
+                    //result.Data
+                    for (const item of result.Data) {
+                        var temp = `
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img class="rounded-circle" src="` + item.ImageUrl + `" style="width: 48px; height: 48px;">
+                                    <span>`+ item.Name + `</span>
+                                </div>
+                                <div class="col-md-6">
+                                    <a name="btnNameEdit" class="btn btn-sm btn-link " type="button">Edit</a>
+                                    <a name="btnImageUpload" class="btn btn-sm btn-link " type="button">Upload</a>
+                                </div>
+                            </div>
+                        `;
+                        self.$elm.find('#divProjectList').append(temp);
+                    }
+
+                } else {
+                    mnNotification.warning(result.Message);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("(" + jqXHR.status + ") " + jqXHR.statusText + "\n" + this.url);
+            }
+        });
     }
 
 
